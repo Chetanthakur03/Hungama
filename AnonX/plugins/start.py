33f096b7565215967f8f5178102dd15741024510
DJ <119585273+Chetanthakur03@users.noreply.github.com>
2023-05-05 23:04:11 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -87,7 +87,7 @@ async def start_comm(client, message: Message, _):
                     details = stats.get(vidid)
                     title = (details["title"][:35]).title()
                     if vidid == "telegram":
-                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/NOOBCREATOR) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
+                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ](https://t.me/rapstar_x3_kitty) ** ᴩʟᴀʏᴇᴅ {count} ᴛɪᴍᴇs**\n\n"
                     else:
                         msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
                 msg = _["ustats_2"].format(tot, tota, limit) + msg
@@ -172,7 +172,7 @@ async def start_comm(client, message: Message, _):
                             text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/NOOBCREATOR"
+                            text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/rapstar_x3_kitty"
                         ),
                     ],
                 ]
@@ -201,7 +201,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgQAAx0CZIiVngABBB6tYvs-dV4rbsUv3h3oKbscOz07ofQAAkkMAAJLUMlT3uEIKksW8bwpBA")
+                await message.reply_sticker("CAACAgUAAxkBAAECzCVkVHsw5aAGmVGmiWuIXInCw85W1AACvAgAAnMYoFY3kX2l8z_TzS8E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
